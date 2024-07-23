7aae47c2eb164edb3e02b0f1c30c6835d40a0422
BLucky-gh <bluckydev@gmail.com>
2024-07-23 19:08:53 +0000

    chore: add spin loop hint for the infinite loops

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 #![no_main]
 #![no_std]
 
-use core::panic::PanicInfo;
+use core::{hint, panic::PanicInfo};
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    loop {}
+    loop {
+        hint::spin_loop();
+    }
 }
 
 #[panic_handler]
 fn panic(_panic: &PanicInfo<'_>) -> ! {
-    loop {}
+    loop {
+        hint::spin_loop();
+    }
 }
