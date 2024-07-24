486ccaca9782071a3680ce8e19ea9abce89b2108
BLucky-gh <bluckydev@gmail.com>
2024-07-24 23:11:12 +0000

    fix: recover crane toolchain override and disable tests

--- nix/repo/packages.nix
+++ nix/repo/packages.nix
@@ -2,18 +2,20 @@
   inputs,
   cell,
 }: let
-  inherit (inputs) std self nixpkgs;
-  inherit (cell.pkgs) crane;
+  inherit (inputs) std self;
+
+  crane = inputs.crane.lib.overrideToolchain cell.pkgs.rust;
 in {
   # sane default for a binary package
   default = crane.buildPackage {
     meta.description = "the primary package";
-    buildInputs = [nixpkgs.cargo-nextest];
     src = std.incl self [
+      "${self}/.cargo/config.toml"
       "${self}/Cargo.lock"
       "${self}/Cargo.toml"
       "${self}/src"
     ];
-    cargoTestCommand = "cargo nextest run --cargo-profile release";
+    doCheck = false; # can't test anyways
+    cargoExtraArgs = "--target ${cell.pkgs.target-triple}";
   };
 }
